cd7779891934a0427467962d6a7ec66a5d0526b7
afriklabprojet <afriklabprojet@gmail.com>
2026-06-07 23:27:45 +0200

    fix(brand): remove dark mode invert filter from admin logo

--- resources/views/filament/brand-logo.blade.php
+++ resources/views/filament/brand-logo.blade.php
@@ -1,8 +1,8 @@
 <div class="flex items-center gap-2">
-    {{-- Logo avec filtre pour s'adapter au mode sombre --}}
-    <img 
-        src="{{ asset('images/logo-rezi.png') }}" 
-        alt="Rezi App Admin" 
-        class="h-10 w-auto dark:brightness-0 dark:invert"
+    {{-- Logo admin sans inversion pour conserver la couleur de marque --}}
+    <img
+        src="{{ asset('images/logo-rezi.png') }}"
+        alt="Rezi App Admin"
+        class="h-10 w-auto"
     >
 </div>
