16d53f14b99d5b2a500898bb96bb92341ecdf830
afriklabprojet <afriklabprojet@gmail.com>
2026-06-19 02:42:44 +0200

    fix(branding): replace placeholder R letter with actual Rezi logo in application-logo component

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -2,13 +2,13 @@
 
 @php
     $sizes = [
-        'small' => 'h-8',
-        'default' => 'h-10',
-        'large' => 'h-12',
+        'small' => 'h-9',
+        'default' => 'h-14',
+        'large' => 'h-16',
     ];
     $sizeClass = $sizes[$size] ?? $sizes['default'];
 @endphp
 
 <div {{ $attributes->merge(['class' => 'flex items-center']) }}>
-    <img loading="lazy" src="{{ asset('images/logo-rezi.png') }}" alt="Rezi App Logo" class="{{ $sizeClass }} w-auto">
+    <img loading="lazy" src="{{ asset('images/logo-rezi.png') }}?v=2" alt="Rezi App Logo" class="{{ $sizeClass }} w-auto">
 </div>
